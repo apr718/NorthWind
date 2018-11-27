80adc01056269b7f247170aeeaf5e45336102d25
apr718 <a.prykhodko71@gmail.com>
2018-11-27 15:06:33 +0300

    aTagHelper has been added

---
+++ NorthWind/TagHelpers/ImageHtmlHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NorthWind.TagHelpers
+{
+    public static class ImageHtmlHelper
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper, int imageId, string linkText)
+            => new HtmlString($"<a href='/images/{imageId}'>{linkText}</a>");
+    }
+}
---
+++ NorthWind/TagHelpers/aTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace NorthWind.TagHelpers
+{
+    public class aTagHelper : TagHelper
+    {
+        private const string NorthwindIdAttributeName = "northwind-id";
+
+        public int NorthwindId { get; set; }
+ 
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.Add("northwind-id", NorthwindId);
+            var content = await output.GetChildContentAsync();
+            var ct = content.GetContent();
+            var target = "/images/" + NorthwindId;
+
+            output.Attributes.SetAttribute("href", target );
+            
+            output.Content.SetContent(target);
+            output.Content.SetHtmlContent(ct);
+        }
+    }
+}

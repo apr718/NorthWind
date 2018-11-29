7f236a8cdfabb50b80a6ac57876ce64ab3ae3c34
apr718 <a.prykhodko71@gmail.com>
2018-11-29 17:22:43 +0300

    Tag module has been fifinshed

--- NorthWind/TagHelpers/aTagHelper.cs
+++ NorthWind/TagHelpers/aTagHelper.cs
@@ -12,15 +12,15 @@ namespace NorthWind.TagHelpers
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
-            output.Attributes.Add("northwind-id", NorthwindId);
-            var content = await output.GetChildContentAsync();
-            var ct = content.GetContent();
-            var target = "/images/" + NorthwindId;
+            //output.Attributes.Add("northwind-id", NorthwindId);
+            //var content = await output.GetChildContentAsync();
+            //var ct = content.GetContent();
+            //var target = "/images/" + NorthwindId;
 
-            output.Attributes.SetAttribute("href", target );
+            //output.Attributes.SetAttribute("href", target );
             
-            output.Content.SetContent(target);
-            output.Content.SetHtmlContent(ct);
+            //output.Content.SetContent(target);
+            //output.Content.SetHtmlContent(ct);
         }
     }
 }

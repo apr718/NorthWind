d05d7411891bf3707c039db024766c9df44ac036
apr718 <a.prykhodko71@gmail.com>
2018-12-11 12:01:02 +0300

    API has been added

---
+++ NorthWind.Api/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.Api.Service.Interfaces;
+using Services.Interfaces;
+
+namespace NorthWind.Api.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IBLService _bLService;
+        private readonly IConfigurationService _configurationService;
+
+        public ProductsController(IBLService bLService, IConfigurationService configurationService)
+        {
+            _bLService = bLService;
+            _configurationService = configurationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            int pageSize = _configurationService.PageSize;
+            var products = await _bLService.GetAllProductsAsync(pageSize);
+            return Ok(products);
+        }
+    }
+}
